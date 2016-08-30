5188b08ee481c8c4d9d7980566896d622fc8c7ae
Kumar Limbu <kumar.limbu@nixplay.com>
2016-08-30 11:24:42 +0800

    [#129426213] Fixed permission issues with push notifications on Android

--- src/android/UniqueDeviceID.java
+++ src/android/UniqueDeviceID.java
@@ -2,36 +2,31 @@ package hu.dpal.phonegap.plugins;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.Manifest;
 import android.content.Context;
-import android.provider.Settings.Secure;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 
-import java.lang.reflect.Method;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.UUID;
 
 public class UniqueDeviceID extends CordovaPlugin {
 
     public static final String TAG = "UniqueDeviceID";
     public CallbackContext callbackContext;
-    public static final int REQUEST_READ_PHONE_STATE = 0;
+    private static String sID = null;
+    private static final String INSTALLATION = "INSTALLATION";
 
-    protected final static String permission = Manifest.permission.READ_PHONE_STATE;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this.callbackContext = callbackContext;
         try {
             if (action.equals("get")) {
-                if(this.hasPermission(permission)){
-                    getDeviceId();
-                }else{
-                    this.requestPermission(this, REQUEST_READ_PHONE_STATE, permission);
-                }
+                getDeviceId();
             }else {
                 this.callbackContext.error("Invalid action");
                 return false;
@@ -44,65 +39,43 @@ public class UniqueDeviceID extends CordovaPlugin {
 
     }
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions,
-                                          int[] grantResults) throws JSONException {
-        if(requestCode == REQUEST_READ_PHONE_STATE){
-            getDeviceId();
+    public synchronized static String id(Context context) {
+        if (sID == null) {
+            File installation = new File(context.getFilesDir(), INSTALLATION);
+            try {
+                if (!installation.exists())
+                    writeInstallationFile(installation);
+                sID = readInstallationFile(installation);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
+        return sID;
+    }
+
+    private static String readInstallationFile(File installation) throws IOException {
+        RandomAccessFile f = new RandomAccessFile(installation, "r");
+        byte[] bytes = new byte[(int) f.length()];
+        f.readFully(bytes);
+        f.close();
+        return new String(bytes);
+    }
+
+    private static void writeInstallationFile(File installation) throws IOException {
+        FileOutputStream out = new FileOutputStream(installation);
+        String id = UUID.randomUUID().toString();
+        out.write(id.getBytes());
+        out.close();
     }
 
     protected void getDeviceId(){
         try {
             Context context = cordova.getActivity().getApplicationContext();
-            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-
-            String uuid;
-            String androidID = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
-            String deviceID = tm.getDeviceId();
-            String simID = tm.getSimSerialNumber();
-
-            if ("9774d56d682e549c".equals(androidID) || androidID == null) {
-                androidID = "";
-            }
-
-            if (deviceID == null) {
-                deviceID = "";
-            }
-
-            if (simID == null) {
-                simID = "";
-            }
-
-            uuid = androidID + deviceID + simID;
-            uuid = String.format("%32s", uuid).replace(' ', '0');
-            uuid = uuid.substring(0, 32);
-            uuid = uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
-
+            String uuid = id(context);
             this.callbackContext.success(uuid);
         }catch(Exception e ) {
             this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
         }
     }
 
-    private boolean hasPermission(String permission) throws Exception{
-        boolean hasPermission = true;
-        Method method = null;
-        try {
-            method = cordova.getClass().getMethod("hasPermission", permission.getClass());
-            Boolean bool = (Boolean) method.invoke(cordova, permission);
-            hasPermission = bool.booleanValue();
-        } catch (NoSuchMethodException e) {
-            Log.w(TAG, "Cordova v" + CordovaWebView.CORDOVA_VERSION + " does not support API 23 runtime permissions so defaulting to GRANTED for " + permission);
-        }
-        return hasPermission;
-    }
-
-    private void requestPermission(CordovaPlugin plugin, int requestCode, String permission) throws Exception{
-        try {
-            java.lang.reflect.Method method = cordova.getClass().getMethod("requestPermission", org.apache.cordova.CordovaPlugin.class ,int.class, java.lang.String.class);
-            method.invoke(cordova, plugin, requestCode, permission);
-        } catch (NoSuchMethodException e) {
-            throw new Exception("requestPermission() method not found in CordovaInterface implementation of Cordova v" + CordovaWebView.CORDOVA_VERSION);
-        }
-    }
 }
